b513b9c225ccfbf86bbf994fe6881f407e7b80f9
nicosrm <86198952+nicosrm@users.noreply.github.com>
2025-02-23 13:20:36 +0100

    Sorted functions according to visibility.

--- example/EmbeddedChessPieces/src/ECPMovement/ECPMovement.cpp
+++ example/EmbeddedChessPieces/src/ECPMovement/ECPMovement.cpp
@@ -6,23 +6,6 @@ ECPMovement::ECPMovement(Dezibot &dezibot, uint movementCalibration)
   ecpSignalDetection(ECPSignalDetection(dezibot)),
   movementCalibration(movementCalibration) {};
 
-void ECPMovement::moveForward(int timeMovement, int timeBreak) {
-    dezibot.motion.move(0, movementCalibration);
-    delay(timeMovement);
-    dezibot.motion.stop();
-    delay(timeBreak);
-};
-
-void ECPMovement::moveToNextField() {
-    const bool hasStartedOnWhite = ecpColorDetection.isWhiteField();
-    bool isCurrentlyOnWhite = hasStartedOnWhite;
-
-    while (isCurrentlyOnWhite == hasStartedOnWhite) {
-        moveForward(FORWARD_TIME, MOVEMENT_BREAK);
-        isCurrentlyOnWhite = ecpColorDetection.isWhiteField();
-    }
-};
-
 void ECPMovement::move(uint numberOfFields) {
     for (size_t i = 0; i < numberOfFields; i++) {
         moveToNextField();
@@ -39,6 +22,27 @@ void ECPMovement::turnRight() {
     int initialAngle = ecpSignalDetection.measureSignalAngle();
     int goalAngle = (initialAngle + 90) % 360;
     rotateToAngle(goalAngle, initialAngle);
+};
+
+// -----------------------------------------------------------------------------
+// PRIVATE FUNCTIONS
+// -----------------------------------------------------------------------------
+
+void ECPMovement::moveForward(int timeMovement, int timeBreak) {
+    dezibot.motion.move(0, movementCalibration);
+    delay(timeMovement);
+    dezibot.motion.stop();
+    delay(timeBreak);
+};
+
+void ECPMovement::moveToNextField() {
+    const bool hasStartedOnWhite = ecpColorDetection.isWhiteField();
+    bool isCurrentlyOnWhite = hasStartedOnWhite;
+
+    while (isCurrentlyOnWhite == hasStartedOnWhite) {
+        moveForward(FORWARD_TIME, MOVEMENT_BREAK);
+        isCurrentlyOnWhite = ecpColorDetection.isWhiteField();
+    }
 };
 
 void ECPMovement::rotateToAngle(int goalAngle, int initialAngle) {
